27a503f91984b617994780b9973de20795dbc675
ffidls <alinka.orehova@gmail.com>
2025-02-23 20:58:06 -0800

    fix: namespace in arg create a new bucket download file

--- main.py
+++ main.py
@@ -3,31 +3,14 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 import argparse
-s = 0
 
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
-bucket_name = "ffodls-s3-demo-bucket-example111111"
-
-
-def check_bucket(name):
-    s3_for_check = boto3.client('s3')
-    try:
-        s3_for_check.head_object(Bucket=name, Key='file-key')
-        return True
-    except ClientError as e:
-        if e.response['Error']['Code'] == '404':
-            return False                                                             
-
-
-def list_buckets(args):
-    # Print out bucket names
-    for bucket in s3.buckets.all():
-        print(bucket.name)
+bucket_name = "ffodls-s3-demo-bucket-example111112"
 
 
 def create_bucket(
-    name=bucket_name, 
+    name=bucket_name,
     region=None):
 
     # Create bucket
@@ -46,12 +29,34 @@ def create_bucket(
     return True
 
 
+def check_bucket(name):
+    """s3_for_check = boto3.client('s3')
+    try:
+        s3_for_check.head_object(Bucket=name)
+        return True
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False"""
+    fl = True
+    for bucket in s3.buckets.all():
+        if str(bucket_name) == name:
+            fl = False
+        # print(bucket.name)
+    create_bucket(name) if not fl else print("lol")
+    return fl
+
+
+def list_buckets(args):
+    # Print out bucket names
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
+
 def upload_file(args):
     file_name = args.file_name
 
-    """if not check_bucket(bucket_name):
-        print("create a new bucket, try again")
-        #create_bucket()"""
+    check_bucket(bucket_name)
 
     object_name = os.path.basename(file_name)
 
@@ -73,27 +78,25 @@ with open("FILE_NAME", "rb") as f:
 """
 
 def download(args):
-    s3 = boto3.client('s3')
-    #s3.download_file('amzn-s3-demo-bucket', 'OBJECT_NAME', 'FILE_NAME')
+    try:
+        s3 = boto3.client('s3')
+        # s3.download_file('amzn-s3-demo-bucket', 'OBJECT_NAME', 'FILE_NAME')
+        name_file = args.file_download
 
-    print("fails which you have:")
-    objects = s3.list_objects_v2(Bucket=bucket_name)
-    for obj in objects['Contents']:
-        print(obj['Key'])
+        print("fails which you have:")
+        objects = s3.list_objects_v2(Bucket=bucket_name)
+        for obj in objects['Contents']:
+            print(obj['Key'])
 
-    name_file = input("enter the name of the file you want to download: ")
-    ind = name_file.index(".")
-    s3.download_file(bucket_name, name_file, f"s3_file{name_file[ind:]}")
-  
+        ind = name_file.index(".")
+        #s3.download_file(bucket_name, name_file, f"s3_{name_file[:ind]}{name_file[ind:]}")
+
+    except Exception:
+        print("no such file")
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Amazon S3")
-    args = parser.parse_args()
     subparsers = parser.add_subparsers(dest="command", help="command options: list, upload, download")
-
-    # check on namespace
-    if str(args) == "Namespace()":
-        parser.print_help()
 
     parser_list = subparsers.add_parser("list", help="list of S3 ")
     parser_list.set_defaults(func=list_buckets)
@@ -103,17 +106,22 @@ def parse_args():
     upload_bucket.set_defaults(func=upload_file)
 
     download_fail = subparsers.add_parser("download", help="download file")
+    download_fail.add_argument("file_download", type=str)
     download_fail.set_defaults(func=download)
 
     return parser.parse_args()
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Amazon S3")
+    subparsers = parser.add_subparsers(dest="command", help="command options: list, upload, download")
     args = parse_args()
 
+    # check on namespace
     if not hasattr(args, "func"):
-        # print("Error: use --help for information")
+        parser.print_help()
         return
+
     args.func(args)
 
 
